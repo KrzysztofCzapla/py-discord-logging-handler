91dda08298407277b04816a8d3de826650de12a2
KrzysztofCzapla <krzys.cz.pl@gmail.com>
2026-04-30 17:09:13 +0200

    Working Adapter

--- discord_logging_handler/constants.py
+++ discord_logging_handler/constants.py
@@ -4,3 +4,23 @@ DISCORD_WEBHOOK_URL_PREFIX = "https://discord.com/api/webhooks/"
 DISCORD_LOGGING_HANDLER_WEBHOOK_URL = "DISCORD_LOGGING_HANDLER_WEBHOOK_URL"
 DISCORD_LOGGING_HANDLER_WEBHOOK_TOKEN = "DISCORD_LOGGING_HANDLER_WEBHOOK_TOKEN"
 DISCORD_LOGGING_HANDLER_WEBHOOK_ID = "DISCORD_LOGGING_HANDLER_WEBHOOK_ID"
+
+# Error Messages
+WRONG_WEBHOOK_URL_ERROR_MESSAGE = "Received wrong URL for the discord webhook."
+NO_WEBHOOK_URL_SET_ERROR_MESSAGE = """
+DiscordLoggingHandlerException: Could not get the webhook URL to send to Discord API.
+
+You must set it in one of those ways:
+1) pass `webhook_url` argument in the main handler function
+2) pass `webhook_id` and `webhook_token` arguments in the main handler function
+3) Set the `DISCORD_LOGGING_HANDLER_WEBHOOK_URL` environmental variable
+4) Set the `DISCORD_LOGGING_HANDLER_WEBHOOK_TOKEN` and `DISCORD_LOGGING_HANDLER_WEBHOOK_ID` environmental variables
+"""
+
+# Requests
+DEFAULT_HEADERS = {
+    "Content-Type": "application/json",
+    "User-Agent": "python-requests/2.31.0",
+    "Accept": "*/*",
+    "Connection": "keep-alive",
+}
--- discord_logging_handler/discord_api_adapter.py
+++ discord_logging_handler/discord_api_adapter.py
@@ -1,18 +1,37 @@
-from dataclasses import dataclass
-from discord_logging_handler.constants import DISCORD_WEBHOOK_URL_PREFIX
-
-
-@dataclass
-class DiscordJSONData:
-    content: str
+import urllib.request
+from dataclasses import asdict
+import os
+import json
+from discord_logging_handler.constants import (
+    DISCORD_WEBHOOK_URL_PREFIX,
+    DISCORD_LOGGING_HANDLER_WEBHOOK_URL,
+    DISCORD_LOGGING_HANDLER_WEBHOOK_TOKEN,
+    DISCORD_LOGGING_HANDLER_WEBHOOK_ID,
+    NO_WEBHOOK_URL_SET_ERROR_MESSAGE,
+    DEFAULT_HEADERS,
+    WRONG_WEBHOOK_URL_ERROR_MESSAGE,
+)
+from discord_logging_handler.models import DiscordJSONData
 
 
 class DiscordAPIAdapter:
     @staticmethod
     def send_webhook(
         body: DiscordJSONData,
+        webhook_url: str | None = None,
+        webhook_id: str | None = None,
+        webhook_token: str | None = None,
     ):
-        pass
+        webhook_url = DiscordAPIAdapter._get_webhook_url(
+            webhook_url, webhook_id, webhook_token
+        )
+        req = urllib.request.Request(
+            webhook_url,
+            data=json.dumps(asdict(body)).encode("utf-8"),
+            headers=DEFAULT_HEADERS,
+            method="POST",
+        )
+        urllib.request.urlopen(req)
 
     @staticmethod
     def _get_webhook_url(
@@ -32,6 +51,16 @@ class DiscordAPIAdapter:
                 webhook_id, webhook_token
             )
             return DiscordAPIAdapter._verify_webhook_url(webhook_url)
+        elif webhook_url := os.environ.get(DISCORD_LOGGING_HANDLER_WEBHOOK_URL):
+            return DiscordAPIAdapter._verify_webhook_url(webhook_url)
+        elif (webhook_id := os.environ.get(DISCORD_LOGGING_HANDLER_WEBHOOK_ID)) and (
+            webhook_token := os.environ.get(DISCORD_LOGGING_HANDLER_WEBHOOK_TOKEN)
+        ):
+            webhook_url = DiscordAPIAdapter._construct_webhook_url_from_id_and_token(
+                webhook_id, webhook_token
+            )
+            return DiscordAPIAdapter._verify_webhook_url(webhook_url)
+        raise ValueError(NO_WEBHOOK_URL_SET_ERROR_MESSAGE)
 
     @staticmethod
     def _construct_webhook_url_from_id_and_token(webhook_id: str, webhook_token: str):
@@ -40,5 +69,5 @@ class DiscordAPIAdapter:
     @staticmethod
     def _verify_webhook_url(webhook_url: str) -> str:
         if not webhook_url.startswith(DISCORD_WEBHOOK_URL_PREFIX):
-            raise ValueError("Received wrong URL for the discord webhook.")
+            raise ValueError(WRONG_WEBHOOK_URL_ERROR_MESSAGE)
         return webhook_url
