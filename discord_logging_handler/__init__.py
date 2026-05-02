50c3364e91539137379f7051f606086fcf1141ac
KrzysztofCzapla <krzys.cz.pl@gmail.com>
2026-05-02 23:34:18 +0200

    change init

---
+++ discord_logging_handler/__init__.py
@@ -0,0 +1,6 @@
+from discord_logging_handler.api import (
+    add_discord_logging_handler as add_discord_logging_handler,
+)
+from discord_logging_handler.input_data import (
+    DiscordHandlerInputData as DiscordHandlerInputData,
+)
